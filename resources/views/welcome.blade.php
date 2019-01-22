10701f8a3d47f6cb3b5dbc492b99a3b3963ea61d
Rasheed Rahman <ra1da35ma@gmail.com>
2019-01-22 06:54:40 +0100

    improve footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,117 +1,106 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>TEXT SUMMARIZER</title>
+    <title>TEXT SUMMARIZER</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
 
-                height: 100vh;
-                margin: 0;
-            }
+            height: 100vh;
+            margin: 0;
+        }
 
-            .full-height {
-                height: 100vh;
-            }
+        .full-height {
+            height: 100vh;
+        }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
 
-            .position-ref {
-                position: relative;
-            }
+        .position-ref {
+            position: relative;
+        }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
 
-            .content {
-                text-align: center;
-            }
+        .content {
+            text-align: center;
+        }
 
-            .title {
-                font-size: 84px;
-            }
+        .title {
+            font-size: 84px;
+        }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
 
-    <div class="top-left links">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="img/img.png" class="css-class" alt="logo">
+<div class="top-left links">
+    <a class="navbar-brand" href="{{ url('/') }}">
+        <img src="img/img.png" class="css-class" alt="logo">
 
-        </a>
-    </div>
+    </a>
+</div>
 
-    <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-
-
-
-
-
-
-
-        {{--<img src="resources/img/children.JPEG" alt="LOGO" title="" ALIGN="center" />--}}
-
-            <div class="content">
-                <div class="title m-b-md">
-                    TEXT SUMMARIZER FOR CHILDREN NEWS RENDERING
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">About the Software</a>
-                    <a href="https://laracasts.com">user's Guide </a>
-                    <a href="https://laravel-news.com">Feedback / Contact us</a>
-                    {{--<a href="https://blog.laravel.com">Blog</a>--}}
-                    {{--<a href="https://nova.laravel.com">Nova</a>--}}
-                    {{--<a href="https://forge.laravel.com">Forge</a>--}}
-                    {{--<a href="https://github.com/laravel/laravel">GitHub</a>--}}
-                </div>
-            </div>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
         </div>
-    </body>
+    @endif
+
+    {{--<img src="resources/img/children.JPEG" alt="LOGO" title="" ALIGN="center" />--}}
+
+    <div class="content">
+        <div class="title m-b-md">
+            TEXT SUMMARIZER FOR CHILDREN NEWS RENDERING
+        </div>
+
+        <div class="links">
+            <a href="https://laravel.com/docs">About the Software</a>
+            <a href="https://laracasts.com">user's Guide </a>
+            <a href="https://laravel-news.com">Feedback / Contact us</a>
+        </div>
+    </div>
+</div>
+</body>
 </html>
