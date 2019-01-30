18503e068a76b6145bd84f6a9bbd2a6b7610cb20
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2019-01-30 14:13:41 +0100

    creation of about & its route n controller

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oyekan Oluwatobi
+ * Date: 1/30/2019
+ * Time: 1:22 PM
+ */
+?>
+
+
+        <!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>TEXT SUMMARIZER</title>
+<body> TOBAINO</body>
+</head>
+</html>
