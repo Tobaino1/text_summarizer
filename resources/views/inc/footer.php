54313e608734f1788b6c4f8ac4e44448a99f177b
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-03-04 03:26:38 +0100

    creation of footer file in inc

---
+++ resources/views/inc/footer.php
@@ -0,0 +1,11 @@
+ <footer class="page-footer font-small blue">
+
+        <!-- Copyright -->
+        <div align="left" style="color: green"> © 2019 All right reserved Computer Science, Lagos State University 2019 
+        </div>
+
+        <div align="right" style="color: green"> Design by Tobaino </div>
+
+        <!-- Copyright -->
+
+    </footer>
