698fef1b759938e5d39f52b7bfe09b3ae117edbb
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-05-17 23:49:25 +0100

    config upd, img

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Text summarizer for children news rendering'),
 
     /*
     |--------------------------------------------------------------------------
