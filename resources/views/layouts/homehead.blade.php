3960d68c6f34d2e401351e3948e8c9bf08465055
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-05-27 23:20:55 +0100

    update homehead

--- resources/views/layouts/homehead.blade.php
+++ resources/views/layouts/homehead.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Text summarizer for children news rendering') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -30,7 +30,7 @@
    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
         <div class="container"> 
              <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+                {{ config('app.name', 'Text summarizer for children news rendering') }}
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false"
