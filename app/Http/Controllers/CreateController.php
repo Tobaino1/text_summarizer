0de824ddbab6b950ed22b3a639ca1461b413c5de
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-07-01 23:34:49 +0100

    create & feedback ctrl modification

---
+++ app/Http/Controllers/CreateController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;  //use for passing/sending data to the server
+use App\Project; //add the model that was created i.e Project
+
+class CreateController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     * authentication //to redirect unathorized access to login
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+   
+    public function create ()
+    {
+        return view ('create');
+    }
+
+    
+    //creating method
+    public function add (Request $request)
+    {
+        
+       // validating form input
+        $this->validate($request, [
+            'title' =>'required',
+            'specification' => 'required',
+            'result' => 'required'
+        ]);
+            //saving the data
+            $projects = new Project;
+            $projects-> title = $request->input('title');
+            $projects-> specification = $request->input('specification');
+            $projects-> result = $request->input('result');
+            $projects->save();
+        return redirect ('admin')->with('success', 'Project saved successfully!') ;
+    }
+
+        //updating method
+    public function update($id)
+    {
+        //getting and passing the data by id
+        $projects = Project::find($id); 
+        return view ('update_admincrud', ['projects' => $projects]);
+    }
+
+        public function edit (Request $request, $id)
+    {
+       // validating form input
+       $this->validate($request, [
+        'title' =>'required',
+        'specification' => 'required',
+        'result' => 'required'
+    ]);
+    
+    //get data in array n display
+    $data = array (
+        'title' => $request->input('title'),
+        'specification' => $request->input('specification'),
+         'result' => $request->input('result')
+    );
+    //update data by id n redirect
+    Project::where('id', $id)->update($data);
+    return redirect ('admin')->with('success', 'Project Updated successfully!') ;
+    }
+
+    //read method
+    public function read (Request $request, $id)
+    {
+        $projects = Project::find($id); 
+        return view ('read_admincrud', ['projects' => $projects]);
+  }
+
+  //delete method
+        public function delete (Request $request, $id)
+        {
+            Project::where('id', $id)->delete();
+    return redirect ('admin')->with('success', 'Project Deleted successfully!') ;
+        }
+}
--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Project;
+use App\Help;
 
 class FeedbackController extends Controller
 {
@@ -10,5 +12,45 @@ class FeedbackController extends Controller
     public function index()
     {
         return view('feedback_contact');
+    }
+
+    public function previous_proj ()
+    { 
+            //using Project model to call/select all the records  
+         $projects = Project::all();
+         return view ('previous_proj', ['projects' => $projects]);
+
+    }
+
+
+    public function help ()
+    {
+        return view ('help');
+    }
+
+
+    //method inserting help request
+    public function insert_help (Request $request)
+    {
+        
+       // validating form input
+        $this->validate($request, [
+            'title' =>'required',
+            'requirement_aimobj' => 'required',
+            'Methodology_technology' => 'required',
+            'email' => 'required',
+            'phone' => 'required'
+
+        ]);
+            //saving the data
+            $helps = new Help;
+            $helps-> title = $request->input('title');
+            $helps-> requirement_aimobj = $request->input('requirement_aimobj');
+            $helps-> Methodology_technology = $request->input('Methodology_technology');
+            $helps-> email = $request->input('email');
+            $helps-> phone = $request->input('phone');
+
+            $helps->save();
+        return redirect ('/')->with('success', 'Your request has been sent successfully!, we will get back to you') ;
     }
 }
