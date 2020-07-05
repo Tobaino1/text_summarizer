4c77a086d4665202d2818e01d331b07603f2a424
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-07-05 18:04:38 +0100

    project model

---
+++ app/Project.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{ 
+	protected $fillable = [		
+		'title',
+		'specification',
+		'result',		
+	];
+}
